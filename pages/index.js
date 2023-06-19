51ab1ba1e9c28df2115b212580c3491358de658a
zerinoid <leo.zerino@gmail.com>
2023-06-18 23:15:03 -0300

    refactor: prettier

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-import Head from "next/head"
-import Layout from "../components/layout"
-import Banner from "../components/banner"
-import Carousel from "../components/carousel"
-import Feedback from "../components/feedback"
-import Equipe from "../components/equipe"
-import Contato from "../components/contato"
+import Head from 'next/head';
+import Layout from '../components/layout';
+import Banner from '../components/banner';
+import Carousel from '../components/carousel';
+import Feedback from '../components/feedback';
+import Equipe from '../components/equipe';
+import Contato from '../components/contato';
 
 export default function Home() {
-    return (
-        <Layout>
-            <Head>
-                <title>Leonardo Zerino</title>
-            </Head>
-            <Banner />
-            <Carousel />
-            <Feedback />
-            <Equipe />
-            <Contato />
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Head>
+        <title>Leonardo Zerino</title>
+      </Head>
+      <Banner />
+      <Carousel />
+      <Feedback />
+      <Equipe />
+      <Contato />
+    </Layout>
+  );
 }
